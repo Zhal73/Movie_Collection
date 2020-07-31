34dc8bf2e12e8ba23551b80f65fbcd93f4fbccde
Domenico Gagliano <domenico.gagliano1973@gmail.com>
2020-07-31 12:38:01 +0000

    completed Read functionality, app can read from the database

--- create.py
+++ create.py
@@ -1,7 +1,11 @@
 # import the SQLAlchemy instance
 from application import db
-# import the definition of the movies table (movies class)
-from application.models import Movies
+# import the definition of the movies table (movies class),
+# actor table(Actors class) and cast_details table (Cast_datails class)
+from application.models import Movies,Actors,Cast_details
+
+#drop all existing tables, if any
+db.drop_all()
 
 #creates the table inside the db
 db.create_all()
