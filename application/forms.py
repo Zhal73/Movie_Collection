b86a8dc6c9658e757cb6bfc4408236971aaaaaf5
Domenico Gagliano <domenico.gagliano1973@gmail.com>
2020-08-01 08:53:34 +0000

    Insert movie page, clean unnecessary code

--- application/forms.py
+++ application/forms.py
@@ -3,7 +3,6 @@ from flask_wtf import FlaskForm
 #import the necessary data type to use in the field
 from wtforms import StringField, IntegerField, DateField,SubmitField
 from wtforms.validators import NumberRange
-from wtforms_components import DateRange
 from datetime import date
 #import the necessary validator to make sure that
 #the data inserted is of the valid format
@@ -19,22 +18,7 @@ class MovieForm(FlaskForm):
                 Length(min=2, max=100)
             ]
         )
-    """
-    release = StringField('Release Year : ',
-            validators = [
-                Length(min=4, max=4)
-                ]
-        )
-    """
-    """
-    release_year = DateField('Release Year : ',
-            validators = [
-                DateRange(
-                max=date.today().year)
-            ]
-        )
-    submit = SubmitField('Add Movie')
-    """
+
     release_year = IntegerField('Release Year : ',
             validators = [
                 NumberRange(
